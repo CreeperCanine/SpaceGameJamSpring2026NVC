70d8892262500244b9a243c68aa5f0a11e3f6a24
gus19-20 <dwgale2025@gmail.com>
2026-03-09 20:08:27 -0500

    Rays and Vectors
    I have been fiddling with vectors, I am not done

--- NVCGameJamSpring2026/Assets/Scripts/AmbushBehavior.cs
+++ NVCGameJamSpring2026/Assets/Scripts/AmbushBehavior.cs
@@ -2,15 +2,20 @@
  This is the Ambush's AI. All of its behavior can be found here
  */
 using System.Collections;
-using System.Collections.Generic;   
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AmbushBehavior : MonoBehaviour
 {
     [SerializeField]
     GameObject Player_Reference; //
-    Vector3 playerLocation; //stores players location as a vector3
+    [SerializeField]
+    GameObject POVStart;
     EnemyMovement Move; //Reference to the movement script on Ambush
+    Vector3 rayStartPoint;
+    RaycastHit[] hitArray = new RaycastHit[4];
+    float[] rayOffsets = {-2, -1, 0, 1, 2};
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +23,22 @@ public class AmbushBehavior : MonoBehaviour
     }
     private void Update()
     {
+        rayStartPoint = POVStart.transform.position;
         transform.LookAt(Player_Reference.transform.position);
-        RaycastHit hit;
-        if(Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f), transform.forward, out hit, Mathf.Infinity)) {
-            Debug.Log(hit.collider) ;
+        Physics.Raycast(rayStartPoint, new Vector3(transform.forward.x - 3f, transform.forward.y - 6f, transform.forward.z -3f), out hitArray[0], Mathf.Infinity);
+        Physics.Raycast(rayStartPoint, new Vector3(transform.forward.x - 1.5f, transform.forward.y - 6f, transform.forward.z - 3f), out hitArray[2], Mathf.Infinity);
+        Physics.Raycast(rayStartPoint, new Vector3(transform.forward.x + 0, transform.forward.y - 6f, transform.forward.z - 3f), out hitArray[2], Mathf.Infinity);
+        Physics.Raycast(rayStartPoint, new Vector3(transform.forward.x + 1.5f, transform.forward.y - 6f, transform.forward.z - 3f), out hitArray[2], Mathf.Infinity);
+        Physics.Raycast(rayStartPoint, new Vector3(transform.forward.x +3f, transform.forward.y - 6f, transform.forward.z - 3f), out hitArray[2], Mathf.Infinity);
+        Debug.DrawRay(rayStartPoint, new Vector3(transform.forward.x - (transform.position.x - 3f), 0, transform.forward.z - transform.position.z) - rayStartPoint);
+        Debug.DrawRay(rayStartPoint, new Vector3(transform.forward.x - (transform.position.x - 1.5f), 0, transform.forward.z - transform.position.z) - rayStartPoint);
+        Debug.DrawRay(rayStartPoint, new Vector3(transform.forward.x - (transform.position.x + 0f), 0, transform.forward.z - transform.position.z) - rayStartPoint);
+        Debug.DrawRay(rayStartPoint, new Vector3(transform.forward.x - (transform.position.x + 1.5f), 0, transform.forward.z - transform.position.z) - rayStartPoint);
+        Debug.DrawRay(rayStartPoint, new Vector3(transform.forward.x -(transform.position.x + 3f), 0, transform.forward.z - transform.position.z) - rayStartPoint);
+        foreach(RaycastHit hit in hitArray)
+        {
+            Debug.Log(hit.collider);
         }
-        Debug.DrawLine(new Vector3(transform.position.x, transform.position.y, transform.position.z + 1f), transform.forward);
     }
     IEnumerator stalkState() 
     {
