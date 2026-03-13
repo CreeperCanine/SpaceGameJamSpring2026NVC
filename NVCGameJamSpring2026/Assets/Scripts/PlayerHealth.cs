76b0edf9f74661998a090c8b1010868b36918188
gus19-20 <dwgale2025@gmail.com>
2026-03-12 23:54:01 -0500

    commit

--- NVCGameJamSpring2026/Assets/Scripts/PlayerHealth.cs
+++ NVCGameJamSpring2026/Assets/Scripts/PlayerHealth.cs
@@ -9,9 +9,7 @@ public class PlayerHealth : MonoBehaviour
     public float totalHealth;
     private float currentOxygen;
     public float totalOxygen;
-
-    public GameObject theoryBar; // The way I understand it we are dealing with a 2D ui element here
-    public GameObject theoryBar2;
+    public GameObject[] oxyBar = new GameObject[6];
     private void Start()
     {
         currentOxygen = totalOxygen;
@@ -30,13 +28,11 @@ public class PlayerHealth : MonoBehaviour
     private void updateHealthBar()
     {
         float valueToChange = totalHealth/100;
-        theoryBar.transform.localScale = new Vector3(0, valueToChange, 0);
     }
 
     private void updateOxyBar()
     {
         float valueToChange = totalHealth / 100;
-        theoryBar.transform.localScale = new Vector3(0, valueToChange, 0);
     }
 
     IEnumerator depleteOxygen()
