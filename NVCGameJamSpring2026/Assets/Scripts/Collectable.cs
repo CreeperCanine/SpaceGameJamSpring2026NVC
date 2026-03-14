c55c792c19d278cc34eface72c231fd571825d90
gus19-20 <dwgale2025@gmail.com>
2026-03-14 06:34:45 -0500

    Commit
    I do not know what the problem with the key area is, I dropped GameObjects tagged "Key" in them and it seemed to work as intended, the gameobject disappeared and the part count increased by 1

--- NVCGameJamSpring2026/Assets/Scripts/Collectable.cs
+++ NVCGameJamSpring2026/Assets/Scripts/Collectable.cs
@@ -10,8 +10,7 @@ public class Collectable : MonoBehaviour, IInteractable
     {
         player = GameObject.FindWithTag("Player"); // Set the Player gameObject tag in the hierarchy to "Player" to get this to work.
         PlayerInventory inventory = player.GetComponent<PlayerInventory>();
-        if(tag == "ShipPart" && inventory.NumberOfParts != inventory.maxShipPart) { inventory.shipPartPickedUp(); gameObject.SetActive(false); }
-        else if(tag == "Berry" && inventory.NumberOfBerry != inventory.maxBerry) { inventory.berryPickedUp(); gameObject.SetActive(false); }
+        if(tag == "Berry" && inventory.NumberOfBerry != inventory.maxBerry) { inventory.berryPickedUp(); gameObject.SetActive(false); }
         else { inventory.pickUpCollectable(); gameObject.SetActive(false); }
     }
 }
--- NVCGameJamSpring2026/Assets/Scripts/PlayerInventory.cs
+++ NVCGameJamSpring2026/Assets/Scripts/PlayerInventory.cs
@@ -5,30 +5,23 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
-    public int NumberOfParts = 0;
     public int NumberOfBerry = 0;
     public int NumberOfCollectables { get; private set; }
     public int maxBerry = 6;
-    public int maxShipPart = 3;
+    public int maxCollectables = 3;
     [SerializeField] private TextMeshProUGUI berryText;
-    [SerializeField] private TextMeshProUGUI partText;
+    [SerializeField] private TextMeshProUGUI collectableText;
 
     private void Start()
     {
         berryText.text = $"Berries\n{NumberOfBerry}/{maxBerry}";
-        partText.text = $"Berries\n{NumberOfParts}/{maxShipPart}";
+        collectableText.text = $"Collectables\n{NumberOfCollectables}/{maxCollectables}";
     }
     public void berryPickedUp()
     {
         NumberOfBerry++;
         berryText.text = $"Berries\n{NumberOfBerry}/{maxBerry}";
         Debug.Log(NumberOfBerry);
-    }
-
-    public void shipPartPickedUp()
-    {
-        NumberOfParts++;
-        partText.text = $"Berries\n{NumberOfParts}/{maxShipPart}";
     }
 
     public void pickUpCollectable() //tbh i'm not sure if collectable is a general term of if theres some other interactable in the game but the option to pick one up is here in case we need it
