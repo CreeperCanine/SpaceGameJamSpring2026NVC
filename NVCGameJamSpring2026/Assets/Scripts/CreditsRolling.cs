9c685c635519f0c4d11ad8d489ccb52632f00be3
wisteria-tiger <dartingsparrow77@icloud.com>
2026-03-14 03:47:52 -0500

    Credits Commit v1

---
+++ NVCGameJamSpring2026/Assets/Scripts/CreditsRolling.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsRolling : MonoBehaviour
+{
+    public float scrollSpeed = 250f;
+
+    private RectTransform rectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+
+
+}
